308ecc91243d4ae9a6cf84656f8ec506621ebc36
ddunvill <ddunvill@ford.com>
2021-10-06 13:16:54 -0400

    Working root-config for 3 applications, also has layout engine to control layout of applications.

--- src/main-root-config.js
+++ src/main-root-config.js
@@ -1,23 +1,41 @@
 import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
 
-registerApplication({
-  name: "@main/pro",
-  app: () => System.import("@main/pro"),
-  activeWhen: ["/"],
+// registerApplication({
+//   name: "@main/root",
+//   app: () => System.import("@main/root"),
+//   activeWhen: ["/"],
+// });
+//
+// registerApplication({
+//   name: "@main/one",
+//   app: () => System.import("@main/one"),
+//   activeWhen: ["/one"],
+// });
+//
+// registerApplication({
+//   name: "@main/two",
+//   app: () => System.import("@main/two"),
+//   activeWhen: ["/two"],
+// });
+//
+// start({
+//   urlRerouteOnly: true,
+// });
+
+const routes = constructRoutes(document.querySelector("#single-spa-layout"));
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
 });
 
-registerApplication({
-  name: "@main/one",
-  app: () => System.import("@main/one"),
-  activeWhen: ["/one"],
-});
+constructLayoutEngine({ routes, applications });
 
-registerApplication({
-  name: "@main/two",
-  app: () => System.import("@main/two"),
-  activeWhen: ["/two"],
-});
-
-start({
-  urlRerouteOnly: true,
-});
+applications.forEach(registerApplication);
+start();
